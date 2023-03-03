bbe4b19619f7f928234d8cc368ad6dd4c8aedf61
luuu-xu <a550763321@gmail.com>
2023-03-03 01:04:23 -0500

    Minor change of indexController.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,13 @@
 const Message = require('../models/message');
 
 // Display index page with a list of messages on GET.
-exports.index_get = async function(req, res, next) {
-  const list_messages = 
-    await Message.find().sort({ timestamp: -1 }).limit(10).populate('author');
-  res.render('index', {
-    user: req.user,
-    message_list: list_messages,
-  });
+exports.index_get = (req, res, next) => {
+  Message.find().sort({ timestamp: -1 }).limit(10).populate('author')
+    .then((list_messages) => {
+      res.render('index', {
+        user: req.user,
+        message_list: list_messages,
+      });
+    })
+    .catch(err => next(err));
 }
